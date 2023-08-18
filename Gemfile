1ff52dd7ff0bd14a3ec3199ea33372d50d72e6ec
cosmosha <cosmoshagan@gmail.com>
2023-08-18 13:33:12 +0000

    Add model Spec folder and files

--- Gemfile
+++ Gemfile
@@ -51,6 +51,11 @@ gem 'bootsnap', require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+
+group :development, :test do
+  gem 'rspec-rails'
+end
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
@@ -73,3 +78,4 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
+
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.new(
+    name: 'Tom',
+    photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+    bio: 'Teacher from Mexico.'
+  ) }
+
+  before { subject.save }
+
+  describe '#validation' do
+    it 'name should be present' do
+      subject.name = 'Tom'
+      expect(subject).to be_valid
+    end
+
+    it 'posts counter should be an integer' do
+      subject.posts_counter = 'none'
+      expect(subject).to_not be_valid
+    end
+
+    it 'posts counter should be greater than 0' do
+      subject.posts_counter = -1
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe '#recent_posts' do
+    it 'should return 3 recent posts' do
+      Post.create(author: subject, title: 'Hello RoR', text: 'Try Test', created_at: 5.days.ago)
+      Post.create(author: subject, title: 'Hello RoR', text: 'Try Test', created_at: 4.days.ago)
+      post1 = Post.create(author: subject, title: 'Hello RoR', text: 'Try Test', created_at: 3.days.ago)
+      post2 = Post.create(author: subject, title: 'Hello RoR', text: 'Try Test', created_at: 2.days.ago)
+      post3 = Post.create(author: subject, title: 'Hello RoR', text: 'Try Test', created_at: 1.days.ago)
+
+      expect(subject.recent_posts.to_a).to eql([post3, post2, post1])
+    end
+  end
+
+end
