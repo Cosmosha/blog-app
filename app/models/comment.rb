5f41b4e7de954e074d5bcf926fdf05d35ecd881a
cosmosha <cosmoshagan@gmail.com>
2023-08-18 02:02:59 +0000

    Add model methods for data

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name:'Post', foreign_key: :post_id
+
+  after_create :update_comments_counter
+  after_destroy :update_comments_counter
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,25 @@
 class Post < ApplicationRecord
-  belongs_to :author
+  has_many :likes
+  has_many :comments, foreign_key: :post_id
+  belongs_to :author, class_name: 'User'
+  validates :title, presence: true, length: { maximum: 550 }
+  validates :comments_counter, numericality: { only_integer: true }
+  validates :likes_counter, numericality: { only_integer: true }
+
+  after_create :increment_user_posts_counter
+  before_destroy :decrement_user_posts_counter
+
+  def increment_user_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def decrement_user_posts_counter
+    author.decrement!(:posts_counter)
+  end
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
+
+
