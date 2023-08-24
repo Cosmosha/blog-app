c96f6d9e428792e30ed3f8ed94836f196dabfa01
cosmosha <cosmoshagan@gmail.com>
2023-08-24 19:48:38 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  #User Controller
+
   resources :users, only: [:index, :show]
 
-  #Posts Controller
   resources :posts, only: [:index, :show]
 
-  #Pages Controller
-  get 'pages/index'
 end
